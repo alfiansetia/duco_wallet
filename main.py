042a1c3130f901edca2e6769322a9146a4dd90f8
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2021-12-20 07:27:34 +0700

    add total
    total miner
    total balance

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 url = "https://server.duinocoin.com/users/"
 
 user = ['alfiansetia', 'alfinetwork', 'alfi']
+btot = 0
+mtot = 0
 
 for x in user:
     get = requests.get(url+x)
@@ -11,5 +13,9 @@ for x in user:
     u = js['result']['balance']['username']
     b = js['result']['balance']['balance']
     m = js['result']['miners']
-    t= f"""balance {u} : {b} \nminers : {len(m)} \n """
+    btot = btot + float(b)
+    mtot = mtot +int(len(m))
+    t = f"""Balance {u} : {b} \nMiners : {len(m)} \n """
     print(t)
+print("Total Miners : " + str(mtot))
+print("Total Balance : " + str(btot))
